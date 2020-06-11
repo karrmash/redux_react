df550d59df018264acd361580e72ff1b3886dc0e
karmash <prof-assistant.yandex.ru>
2020-06-11 15:13:01 +0800

    Завершил курс реакт, редакс.

---
+++ src/redux/actions.js
@@ -0,0 +1,60 @@
+import {CREATE_POST, FETCH_POST, HIDE_ALERT, HIDE_LOADER, REQUEST_POSTS, SHOW_ALERT, SHOW_LOADER} from "./types";
+
+export function createPost(post) {
+    return {
+        type: CREATE_POST,
+        payload: post
+    }
+}
+
+export function fetchPosts () {
+    return {
+        type: REQUEST_POSTS
+    }
+    // return async dispatch => {
+    //     try {
+    //         dispatch(showLoader())
+    //         const response = await fetch('https://jsonp1laceholder.typicode.com/posts?_limit=5')
+    //         const json = await response.json()
+    //
+    //         dispatch({type: FETCH_POST, payload: json})
+    //         setTimeout(()=>{   dispatch(hideLoader())}, 1000)
+    //     } catch (e) {
+    //         dispatch(showAlert("Что-то пошло не так"))
+    //         dispatch(hideLoader())
+    //     }
+    //
+    // }
+}
+
+export function showLoader() {
+    return {
+        type: SHOW_LOADER
+    }
+}
+
+export function hideLoader() {
+    return {
+        type: HIDE_LOADER
+    }
+}
+
+
+export function showAlert(text) {
+    return dispatch => {
+        dispatch({
+            type: SHOW_ALERT,
+            payload: text
+        })
+
+        setTimeout(()=>{
+            dispatch(hideAlert())
+        }, 3000);
+    }
+}
+
+export function hideAlert() {
+    return {
+        type: HIDE_ALERT
+    }
+}
